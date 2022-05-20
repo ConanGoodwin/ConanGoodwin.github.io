ff5906579018d8efde65964dff1f5e10ecea2a88
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-20 01:51:53 -0300

    att

--- minhacasa/codigo.js
+++ minhacasa/codigo.js
@@ -1,7 +1,72 @@
-function escondeDivOnibus() {
-  let divCarro = document.getElementById('onibus');
+let divOnibusOculto = document.getElementById("onibusOculto");
+divOnibusOculto.style.display = "none";
+let divCarroOculto = document.getElementById("carroOculto");
+divCarroOculto.style.display = "none";
 
-  divCarro.style.display = "none";
+function escondeDiv(transporte) {
+  let divTransporte = document.getElementById(transporte);
+  let divOculto = document.getElementById(transporte + "Oculto");
+  let divTransporteInverso;
+  let divOcultoInverso;
+  let transporteInverso;
 
-  console.log("oi");
+  if (transporte === "onibus") {
+    divTransporteInverso = document.getElementById("carro");
+    divOcultoInverso = document.getElementById("carroOculto");
+    transporteInverso = "carro";
+  } else {
+    divTransporteInverso = document.getElementById("onibus");
+    divOcultoInverso = document.getElementById("onibusOculto");
+    transporteInverso = "onibus";
+  }
+
+  if (divTransporte.style.display == "") {
+    atualizaOlho(transporte + "Olho","./Icones/invisivel.png");
+    divTransporte.style.display = "none";
+    divOculto.style.display = "";
+
+    atualizaOlho(transporteInverso + "Olho","./Icones/visivel.png");
+    divTransporteInverso.style.display = "";
+    divOcultoInverso.style.display = "none";
+  }
+}
+
+function mostraEscondeDiv(transporte) {
+  let divTransporte = document.getElementById(transporte);
+  let divOculto = document.getElementById(transporte + "Oculto");
+
+  if (mostraEscondeOlho(transporte + "Olho")) {
+    divTransporte.style.display = "none";
+    divOculto.style.display = "";
+  } else {
+    divTransporte.style.display = "";
+    divOculto.style.display = "none";
+  }
+}
+
+function atualizaOlho(tipoOlho, caminho) {
+  let iconeOlho = document.getElementsByClassName(tipoOlho);
+  let caminhoIcone = iconeOlho[0].src;
+
+  for (let chave in iconeOlho) {
+    iconeOlho[chave].src = caminho;
+  }
+}
+
+function mostraEscondeOlho(tipoOlho) {
+  let iconeOlho = document.getElementsByClassName(tipoOlho);
+  let caminhoIcone = iconeOlho[0].src;
+
+  if (caminhoIcone.indexOf("/in") === -1) {
+    // iconeOlho[chave].src = "./Icones/invisivel.png";
+    atualizaOlho(tipoOlho, "./Icones/invisivel.png");
+  } else {
+    // iconeOlho[chave].src = "./Icones/visivel.png";
+    atualizaOlho(tipoOlho, "./Icones/visivel.png");
+  }
+
+  if (caminhoIcone.indexOf("/in") === -1) {
+    return true;
+  }
+  return false;
 }
